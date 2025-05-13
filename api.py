861149d3571c3175db558796bf28a6b030615747
Yasith Gunawardhana <gunawardhanayasith@gmail.com>
2025-05-13 16:37:26 +0530

    feat: user searching feature

--- api.py
+++ api.py
@@ -21,13 +21,37 @@ user_args = reqparse.RequestParser()
 user_args.add_argument('name', type=str, required=True, help='Name cannot be blank')
 user_args.add_argument('email', type=str, required=True, help='Email cannot be blank')
 
+user_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'email': fields.String
+}
+
 class Users(Resource):
+    @marshal_with(user_fields)
     def get(self):
         users = UserModel.query.all()
         return users
     
-api.add_resource(Users, '/api/users/')
+    @marshal_with(user_fields)
+    def post(self):
+        args = user_args.parse_args()
+        user = UserModel(name=args['name'], email=args['email'])
+        db.session.add(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return users, 201
 
+class User(Resource):
+    @marshal_with(user_fields)
+    def get(self, id):
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404, f"User with {id} not found")
+        return user
+
+api.add_resource(Users, '/api/users/')
+api.add_resource(User, '/api/users/<int:id>')
 
 @app.route('/')
 def home():
