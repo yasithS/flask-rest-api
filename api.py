4faa301e34ef41883f3315935b4911219c37e2f7
Yasith Gunawardhana <gunawardhanayasith@gmail.com>
2025-05-13 14:40:02 +0530

    feat: api/users/ endpoint

--- api.py
+++ api.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
 db = SQLAlchemy(app)
+api = Api(app)
 
 
-class User(db.Model):
+class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
@@ -14,6 +16,18 @@ class User(db.Model):
     def __repr__(self):
         return f'User({self.name}, {self.email})'
     
+
+user_args = reqparse.RequestParser()
+user_args.add_argument('name', type=str, required=True, help='Name cannot be blank')
+user_args.add_argument('email', type=str, required=True, help='Email cannot be blank')
+
+class Users(Resource):
+    def get(self):
+        users = UserModel.query.all()
+        return users
+    
+api.add_resource(Users, '/api/users/')
+
 
 @app.route('/')
 def home():
