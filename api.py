350aa69d9473866a97688df6508bf328fd7f681e
Yasith Gunawardhana <gunawardhanayasith@gmail.com>
2025-05-13 13:07:37 +0530

    feat: database model

--- api.py
+++ api.py
@@ -6,10 +6,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
 db = SQLAlchemy(app)
 
 
-class User(db.model):
-    id = db.column(db.Integer, primary_key=True)
-    name = db.column(db.String(80), unique=True, nullable=False)
-    email = db.column(db.String(80), unique=True, nullable=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self):
         return f'User({self.name}, {self.email})'
